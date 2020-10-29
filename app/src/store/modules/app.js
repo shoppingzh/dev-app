ac90e42d33354b62d05189b779998b96a49fdaf3
shoppingzh <1049262057@qq.com>
2020-10-29 10:31:58 +0800

    初始化项目

---
+++ app/src/store/modules/app.js
@@ -0,0 +1,20 @@
+const state = {
+  name: '前端开发者平台'
+}
+
+const getters = {
+  name: state => state.name
+}
+
+const mutations = {}
+
+const actions = {}
+
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+}
---
+++ app/src/views/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ appName }}</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters({
+      appName: 'app/name'
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
