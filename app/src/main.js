9442a1ec23342eecf6591f835076a2d4384ade14
shoppingzh <1049262057@qq.com>
2020-11-15 16:34:50 +0800

    'UI初步'

--- app/src/main.js
+++ app/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/plugins/antd'
+
+import '@/styles/index.less'
 
 Vue.config.productionTip = false
 
--- app/src/views/home/index.vue
+++ app/src/views/home/index.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>{{ appName }}</div>
+  <div class="page">
+    <a-row type="flex" align="middle" class="banner">
+      <a-col :span="18" class="banner__slogan">
+        We are <strong>creating</strong> a new vision for your <strong>products!</strong>
+      </a-col>
+      <a-col :span="6" class="banner__pic">
+        <img :src="pic">
+      </a-col>
+    </a-row>
+    <div class="card"></div>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 
 export default {
+  data() {
+    return {
+      pic: 'https://cdn.dribbble.com/users/2760253/screenshots/14579355/media/cd7121e29d37d4eb1311f54e8104abfe.jpg'
+    }
+  },
   computed: {
     ...mapGetters({
       appName: 'app/name'
@@ -14,6 +29,27 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  .page {
+    padding: 30px;
+  }
+  .banner {
+    padding: 0 20%;
+    color: #fff;
+    &__slogan {
+      padding: 100px;
+      font-size: 32px;
+    }
+    &__pic {
+      img {
+        width: 100%;
+        height: auto;
+      }
+    }
+  }
+  .card {
+    width: 300px;
+    height: 200px;
+    background-image: linear-gradient(to right, lighten(#ea5648, 20%), #ea5648);
+  }
 </style>
